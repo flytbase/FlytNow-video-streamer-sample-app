e78090093a90cee2300d3eb63655b826b63ce0cf
Abhay Singh Thakur <abhay@flytbase.com>
2023-12-15 18:55:28 +0530

    feat(on-prem): handled environment setting

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { StateService } from './state.service';
+import { setEnvironment } from '@flytnow/video-client-staging';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(public State:StateService) {
-    
+  constructor(public State: StateService) {
+    // @TODO: set your URL here
+    // setEnvironment(false, 'your url here');
+    setEnvironment(true);
   }
 }
